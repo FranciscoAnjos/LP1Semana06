c1e295b9ae6ae27fe7c07d15eedcb6a8200ed93b
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-28 14:38:21 +0000

    finish stopwatch

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+
 
 namespace Chronos
 {
@@ -8,6 +10,22 @@ namespace Chronos
         {
             Stopwatch chrono1 = new Stopwatch();
             Stopwatch chrono2 = new Stopwatch();
+
+            chrono1.Start();
+            Thread.Sleep(500); 
+
+            chrono2.Start();
+            Thread.Sleep(250);
+
+            chrono1.Stop();
+            chrono2.Stop();
+            
+            double totSeconds0 = chrono1.Elapsed.TotalSeconds;
+            double totSeconds1 = chrono2.Elapsed.TotalSeconds;
+                        
+            Console.WriteLine($"{totSeconds0:f4}");
+            Console.WriteLine($"{totSeconds1:f4}");
+
         }
     }
 }
