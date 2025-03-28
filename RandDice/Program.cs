caf5d25e09d4b35e64294c03ec3dedc3771bcacc
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-28 14:49:44 +0000

    Add RandDice

---
+++ RandDice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random r1 = new Random(int.Parse(args[1]));
+
+            int sumRand = 0;
+
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {  
+                sumRand += r1.Next(1, 7);
+            }
+
+            Console.WriteLine(sumRand);            
+        }
+    }
+}
