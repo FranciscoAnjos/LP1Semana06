dc92d9d83c95b51501e384e2b8a25a636fe8780f
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-28 14:28:43 +0000

    add Hardness and nrooms

--- MyRPG/GameLevel.cs
+++ MyRPG/GameLevel.cs
@@ -1,12 +1,25 @@
 using System;
-
+using Enemy;
 namespace MyRPG
 {
-    public class Enemy
+    public class GameLevel
     {
-        private string name;
-        private float health;
-        private float shield;
+        Enemy[] nRoom;
+        Hardness difficulty;
+
+        public GameLevel (int nRoom , Hardness difficulty)
+        {
+            this.nRooms = new Enemy[nRooms];
+            this.difficulty = difficulty;
+        }
+        
+        public void SetEnemyInRoom(int RoomInd,Enemy enemy )
+        {
+            nRoom[RoomInd] = enemy
+        }
+        public Hardness GetHardness() => difficulty;
+        public int GetNumRooms() => nRooms.Length;
+
     
     }
 
